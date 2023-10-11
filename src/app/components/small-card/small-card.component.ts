a2452a428a4899cdfd018ceaaf9e9830114b6eef
eudes <eudestsilva@gmail.com>
2023-10-10 22:40:22 -0300

    Components Dinamycs Propertys

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
   templateUrl: './small-card.component.html',
   styleUrls: ['./small-card.component.css']
 })
-export class SmallCardComponent {
+export class SmallCardComponent implements OnInit {
+@Input()
+photoCover: string = "https://miro.medium.com/v2/resize:fit:1358/0*qdHImq1G588SB9Ii.jpg"
+@Input()
+cardTitle: string = "NOVA SERIE ANUNCIADA"
 
+constructor() { }
+  ngOnInit(): void {
+
+  }
 }
